416c09fffd4a66c0c26782b19be7ef35ebb1637a
ErsteMaster <testo@testo.tt>
2025-11-21 17:37:32 +0500

    authFetch edit

--- template/client/core/services/connection/fetchRequest.ts
+++ template/client/core/services/connection/fetchRequest.ts
@@ -56,7 +56,7 @@ export interface FetchResponse<T = any> {
     response?: T | null;
     message?: string | null;
     errors?: any[] | null;
-	raw?: Response;
+	raw: Response;
 };
 
 export interface ServerResponse<T = any> {
